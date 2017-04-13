49b44c1e866cf87cd94486dcd3ff6b60b1245682
Nikita Makarov <nikjk495@gmail.com>
2017-04-14 01:04:14 +0300

    Add Nav-bar

--- menu.php
+++ menu.php
@@ -1,16 +1,19 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/lib/includes/database.php');
-
+    include_once ($_SERVER['DOCUMENT_ROOT'] . '/lib/includes/header.php.');
     function   buidMenu($menuArray)
     {
+        echo '
+            <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div  class="collapse navbar-collapse">
+    <ul class="nav navbar-nav"> 
+    ';
         foreach ($menuArray as $node)
         {
             echo "<li><a href='#' />" . $node['title'] . "</a>";
             if ( !empty($node['children']))
             {
-                // TODO: Write function to generate DOM-three and add Bootstap navbar
-
-                echo "<ul>";
                 buidMenu($node['children']);
                 echo "</ul>";
             }
