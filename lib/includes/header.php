bb2c691d1c68d8dfb6c7c8a0f238f0864e80ecb9
Nikita Makarov <nikjk495@gmail.com>
2017-04-14 22:06:23 +0300

    add charset

--- lib/includes/header.php
+++ lib/includes/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf8_general_ci">
+    <meta charset="utf-8">
     <title>  </title>
     <link  rel="stylesheet" type="text/css" href="/todolist/css/bootstrap.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
