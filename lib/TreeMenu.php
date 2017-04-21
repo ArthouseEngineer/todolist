ee902ede41e280b3f288ac8bf4dd696d37e0f30a
Nikita Makarov <nikjk495@gmail.com>
2017-04-21 21:37:08 +0300

    The function of generating a hierarchical list is written, Add dropdown menu

--- lib/TreeMenu.php
+++ lib/TreeMenu.php
@@ -4,39 +4,102 @@
 /**
 * Построение дерева
 */
-class TreeMenu {
-	private $_db = null;
-	private $_category_arr = array ();
+class TreeMenu
+{
+    /**
+     * @_db Дескриптор подключения к БД
+     */
+    private $_db = null;
 
-	public function _construct($db) {
-		// Подключаемся к базе данных и записывем подключение в переменную _db
-		$this -> _db = $db;
-		// В переменную $_category_arr записывем все категории см дальше
-		$this ->_category_arr = $this -> _getCategory();
-	}
-	// Метод читает из таблицы category все строчки и возвращает двумерный массив, в котором первый ключ - id - родителя категория (parent_id) @return array
+    /**
+     * @_categoryArr Массив категорий
+     */
+    private $_categoryArr = null;
 
-	private function _getCategory() {
-		$query = $this->_db->prepare ("SELECT * from `menu`"); //Готовим запрос
-		$query->execute(); //Выполняем запрос
-		// Читаем все строчки и записывем в переменную $result
-        $result = $query->fetchALL(PDO::FETCH_OBJ);
-		$return = array();
-		foreach ($result as $value) {
-			$return[$value->parent_id] []=$value;
-		}
-		return $return;
-	}
+    /**
+     * @_categoryTree Массив Категорий иерархический
+     */
+    private  $_categoryTree = null;
 
-	public function outTree ($parent_id, $level) {
-		if (isset ($this->_category_arr[$parent_id])) {
-			foreach ($this->_category_arr[$parent_id] as $value) {
-				echo "<div style='margin-left:" . ($level *25) . "'px;'>" . $value->title . "</div>";
-				$level++;
-				$this->outTree($value-id, $level);
-				$level--;
-			}
-		}
-	}
+    public function __construct($db)
+    {
+        // Подключаемся к базе данных и записывем подключение в переменную _db
+        $this->_db = $db;
+
+        // В переменную $_category_arr записывем все категории
+        $this->_categoryArr = $this -> _getCategory();
+
+        //@_categoryTree Иерархический массив категорий с вложенными подпунктам
+        $this->_categoryTree = $this->buildTree($this->_categoryArr);
 }
+      public  function buildTree(array $elements,$parent_id = 0)
+        {
+            $branch = array();
+
+            foreach ($elements as $element)
+            {
+                if ($element['parent_id'] == $parent_id)
+                {
+
+                    $children = $this->buildTree($elements,$element['id']);
+                    if ($children)
+                    {
+                        $element['children'] = $children;
+                    }
+                    $branch[$element['id']] = $element;
+
+                    unset($elements[$element['id']]);
+                }
+            }
+            return $branch;
+        }
+
+    public function  getMenuHtml()
+    {
+        echo '
+            <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
+  <div class="navbar-header">
+  <a class="navbar-brand" href="#">ToDoList</a>
+    <div  class="collapse navbar-collapse navbar-ex1-collapse">
+    </div>
+    </div>
+    <ul class="nav navbar-nav">
+    ';
+
+        $this->buidMenu($this->_categoryTree);
+       /* echo '<pre>';
+        print_r($this->_categoryTree); */
+    }
+
+    public function buidMenu($nodes)
+    {
+        foreach ($nodes as $node) {
+            if (empty($node['children']))
+            {
+                echo '<li><a href="#" />' . $node['title'] . "</a>";
+            }
+             else
+            {
+                echo '       <li class="dropdown">';
+               echo' <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
+                        aria-expanded="false">' . $node['title'] . ' <span class="caret"></span>';
+                echo '<ul class="dropdown-menu">';
+                $this->buidMenu($node['children']);
+                echo '</ul>';
+            }
+            echo '</li>';
+
+        }
+    }
+
+    private function _getCategory()
+    {
+        $query = $this->_db->prepare("SELECT * FROM menu WHERE active=1");
+        $query->execute();
+
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+       return $result;
+    }
+}
+
 ?>
--- menu.php
+++ menu.php
@@ -1,30 +1,7 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/lib/includes/database.php');
-    include_once ($_SERVER['DOCUMENT_ROOT'] . '/lib/includes/header.php.');
-    function   buidMenu($menuArray)
-    {
-        echo '
-            <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
-  <div class="navbar-header">
-  <a class="navbar-brand" href="#">ToDoList</a>
-    <div  class="collapse navbar-collapse navbar-ex1-collapse">
-    </div>
-    </div>
-    <ul class="nav navbar-nav"> 
-    ';
-        foreach ($menuArray as $node)
-        {
-            echo "<li><a href='#' />" . $node['title'] . "</a>";
-            if ( !empty($node['children']))
-            {
-                buidMenu($node['children']);
-                echo "</ul>";
-            }
-            echo "</li>";
-        }
-    }
-        $query = $dbh->prepare("SELECT * FROM menu WHERE active=1");
-        $query->execute();
+    include_once ($_SERVER['DOCUMENT_ROOT'] . '/lib/includes/header.php');
+    include_once($_SERVER['DOCUMENT_ROOT'] . '/lib/TreeMenu.php');
 
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        buidMenu($result);
+$tree = new TreeMenu($dbh);
+$tree->getMenuHtml();
